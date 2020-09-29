86703d96bcef1029c27654e509034449fffa71f0
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-29 00:06:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,8 +3,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  hash = {name: "Steve"}
-  hash[:key] 
+  {name: "Steve"}
+  hash[:name] 
 end
 
 def update_counting_hash(hash, key)
